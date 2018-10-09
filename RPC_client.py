99a6d2993f54d251947b56548c5a7286d4f64f90
qmtran36 <43387187+qmtran36@users.noreply.github.com>
2018-10-09 09:10:39 -0400

    creates a socket

--- RPC_client.py
+++ RPC_client.py
@@ -1,16 +1,17 @@
-import csv
 import sys
+import socket
 
 ipAddress = sys.argv[1]
-tcpPort = sys.argv[2]
+tcpPort = int(sys.argv[2])
 
-def main():
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	print(ipAddress)
-	print(tcpPort)
+cSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+cSocket.connect((ipAddress, tcpPort))
 
-	#Post Game message
-	print ("Disconnecting from the game server. Thank you for playing!\n")
+print(ipAddress)
+print(tcpPort)
+
+#Post Game message
+print ("Disconnecting from the game server. Thank you for playing!\n")
 
 def ClientA():#Client A or Player 1
 	print ("Connected to the Rock, Paper, Scissors Game Server.")
--- RPC_server.py
+++ RPC_server.py
@@ -1,36 +1,71 @@
-import csv
 import sys
+#from socket import *
+import socket
 
-port = sys.argv[1]
-
+port = int(sys.argv[1])
+#create a socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print(port)
-def main():
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	#Starting game server
-	print ("Server is ready to serve!\n")
+# hostname = socket.gethostname() 
+# IPAddr = socket.gethostbyname(hostname) 
+#Starting game server
+s.bind((socket.gethostname(),port))
+s.listen(1)
 
-#Client A connected as player 1
-print ("A new player is trying to connect to the game server on ")
-print ("Current number of available players is 1")
-print ("One player waiting for another player to join\n")
+print ("Server is ready to serve!\n")
+while 1:
+	cSocket, address = s.accept()
+	char = connectionSocket.recv(1024)
+	cSocket.send(char)
+	cSocket.close()
+#addr_size = sizeof their_addr;
+#new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
 
-#Client B connected as player 2
-print ("A new player is trying to connect to the game server on ")
-print ("Current number of available players is 2")
+#calls()
 
-#Game begin Client A and B begins
-print ("New Game Thread spawned")
+#def main():
+	# #create a socket
+	# serverSocket = socket(AF_INET, SOCK_STREAM)
+	# #hostname = socket.gethostname() 
+	# #IPAddr = socket.gethostbyname(hostname) 
+	# #print(hostname)
+	# #print(IPAddr)
+	# #Starting game server
+	# serverSocket.bind(port)
+	# serverSocket.listen(1)
 
-#Client A chose R, P or S
-print ("Player 1 played: \n")
+	# #addr_size = sizeof their_addr;
+	# #new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
+	# print ("Server is ready to serve!\n")
+	# #calls()
 
-#Client C is trying to connect while server is busy.
-print ("A new player is trying to connect to the game server on ")
-print ("Two players are already. Can't start a new game.\n")
+# #Client A connected as player 1
+# def calls():
+# 	print ("A new player is trying to connect to the game server on ")
+# 	print ("Current number of available players is 1")
+# 	print ("One player waiting for another player to join\n")
 
-#Client B chose R, P or S
-print ("Player 2 played: \n")
+# 	#Client B connected as player 2
+# 	print ("A new player is trying to connect to the game server on ")
+# 	print ("Current number of available players is 2")
 
-#Game begin Client A and B continues and finishes.
-print ("Player # won!")
-print ("Game over.")
+# 	#Game begin Client A and B begins
+# 	print ("New Game Thread spawned")
+
+# 	#Client A chose R, P or S
+# 	print ("Player 1 played: \n")
+
+# 	#Client C is trying to connect while server is busy.
+# 	print ("A new player is trying to connect to the game server on ")
+# 	print ("Two players are already. Can't start a new game.\n")
+
+# 	#Client B chose R, P or S
+# 	print ("Player 2 played: \n")
+
+# 	#Game begin Client A and B continues and finishes.
+# 	print ("Player # won!")
+# 	print ("Game over.")
+
+
+# if __name__ == "__main__":
+#     main()
